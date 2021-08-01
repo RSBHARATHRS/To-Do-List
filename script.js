ddd7154bb1c2b078504b1b84d44cda2de2406e56
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2021-08-01 01:04:15 -0700

    File added

---
+++ script.js
@@ -0,0 +1,53 @@
+let add_btn = document.getElementById("add-task-button");
+let input_task = document.getElementById("input-task");
+let ul = document.getElementById("task-list");
+let arr=[];
+arr=JSON.parse(localStorage.getItem("tasks")) || [];
+
+for(let i=0;i<arr.length;i++){
+    createLi(arr[i]);
+    console.log("Locally saved task "+i+" :"+arr[i]);
+}
+
+
+add_btn.addEventListener("click",function (){
+    let val=input_task.value;
+    createLi(val);
+    arr.push(val);
+    localStorage.setItem("tasks", JSON.stringify(arr));
+    console.log("New task added: " + val );
+})
+
+
+function createLi(task){
+    let li = document.createElement("li");
+    let inp=document.createElement("input");
+    let span = document.createElement("span");
+    let txtNode = document.createTextNode(task);
+    let btn = document.createElement("button");
+
+
+    span.appendChild(txtNode);
+    span.setAttribute("class","task");
+    inp.setAttribute("type","checkbox");
+    btn.setAttribute("class","delete-btn");
+    btn.appendChild(document.createTextNode("X"));
+    //btn.appendChild(document.createTextNode("X"));
+    li.appendChild(inp);
+    li.appendChild(span);
+    //li.appendChild(txtNode);
+    li.appendChild(btn);
+
+    btn.addEventListener("click",function (){
+        let temp=arr.splice(arr.indexOf(task),1);
+        console.log("removed task : "+ temp);
+        localStorage.setItem("tasks", JSON.stringify(arr));
+        li.remove();
+    })
+
+    ul.appendChild(li);
+    localStorage.setItem("tasks", JSON.stringify(arr));
+    //li.innerText=input_task.value;
+
+    input_task.value="";
+}
